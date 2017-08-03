a2d206ece23668b61b5c3e6dd2c4fce021e90c26
BOSS-PC\boss <contact@eplex.eu>
2017-08-03 12:19:16 +0200

    overlay

--- _dev/js/skrypty.js
+++ _dev/js/skrypty.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
 	
+	$('.openBtn').on('click', function(){
+		$('.overlay').addClass('openOverlay animated slideInLeft');
+		console.log('klik');
+	});
+
+	$('.closeBtn').on('click', function(){
+		$('.overlay').addClass('openOverlay animated slideOutLeft');
+		console.log('klik');
+	});
 
 });
